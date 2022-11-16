7b1536787ad2c272355de0356baeaf45cef9ab58
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-11-16 18:44:17 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/scss/global.scss";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="leftSquare"></div>
+      <div className="centerSquare"></div>
+      <div className="rightSquare"></div>
+    </div>
+  );
+};
+
+export default App;
